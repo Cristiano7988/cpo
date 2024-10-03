d912f7895a0b9aa2996ee1fdc79709fd22abf868
Cristiano7988 <cristianooliveiramorales@gmail.com>
2024-10-03 20:09:52 -0300

    Adicionando validações no FE

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,12 +1,28 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+
 const router = useRouter()
 const email = ref('')
 const password = ref('')
+const error = ref('')
 
 const auth = () => {
-  localStorage.setItem('token', email.value + password.value)
+  error.value = ''
+
+  if (!email.value) return (error.value = 'Insira um email')
+  if (!/@/.test(email.value)) return (error.value = 'Formato de e-mail inválido')
+
+  const [local, dominio] = email.value.split('@')
+  if (!local) return (error.value = 'Formato de e-mail inválido')
+  if (!dominio) return (error.value = 'Email sem domínio, insira um email válido')
+  if (local.length <= 2 || dominio.length <= 2)
+    return (error.value = 'Email muito curto, utilize outro email')
+
+  if (password.value == '') return (error.value = 'Insira uma senha')
+  if (password.value.length <= 8) return (error.value = 'Senha muito curta')
+
+  localStorage.setItem('token', email.value + password.value) // Aqui salvamos o token na LocalStorage após resposta do backend, o mesmo token será validado validado na middleware das rotas protegidas
   location.pathname = '/about'
 }
 </script>
@@ -17,6 +33,7 @@ const auth = () => {
     <input type="email" v-model="email" placeholder="Email" />
     <input type="password" v-model="password" placeholder="Password" />
     <button @click="auth">Vai Pokebola</button>
+    <i v-if="error">{{ error }}</i>
   </div>
 </template>
 
@@ -30,5 +47,8 @@ const auth = () => {
 }
 h1 {
   text-transform: capitalize;
+}
+i {
+  color: firebrick;
 }
 </style>
