2c785519d28c4f438a53b6025888bed06221df9a
Cristiano7988 <cristianooliveiramorales@gmail.com>
2024-10-03 22:42:50 -0300

    Adicionando navegação na listagem

--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,19 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const pokemons = ref([])
 const offset = ref(1)
 
-const interval = {
-  offset,
-  limit: 10
+const getPokemons = (interval) => {
+  const P = new Pokedex.Pokedex(interval)
+
+  P.getPokemonsList().then((response) => {
+    pokemons.value = response.results
+    return pokemons
+  })
 }
 
-const P = new Pokedex.Pokedex(interval)
+getPokemons({
+  offset,
+  limit: 10
+})
 
-P.getPokemonsList().then((response) => {
-  pokemons.value = response.results
-  return pokemons
+const avanca = () => offset.value++
+const volta = () => offset.value--
+
+watch(offset, (newOffset) => {
+  getPokemons({
+    offset: newOffset,
+    limit: 10
+  })
 })
 </script>
 
@@ -24,6 +36,10 @@ P.getPokemonsList().then((response) => {
       <div v-for="(pokemon, index) in pokemons" :key="index">
         {{ pokemon.name }}
       </div>
+    </div>
+    <div class="navigator">
+      <b v-if="offset > 1" @click="volta">Voltar</b>
+      <b v-if="pokemons.length" @click="avanca">Avançar</b>
     </div>
   </div>
 </template>
@@ -36,5 +52,14 @@ P.getPokemonsList().then((response) => {
     justify-content: center;
     flex-direction: column;
   }
+}
+.navigator {
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+}
+
+b {
+  cursor: pointer;
 }
 </style>
